102c1f72fca7f990ab728411f6325aaf1d708479
knunke <60415897+knunke@users.noreply.github.com>
2020-02-04 18:32:54 -0600

    Updates AD User Password

---
+++ Update-AdPassword.ps1
@@ -0,0 +1,9 @@
+$Domain = 'knunke.com'
+$UserToChange = 'knunke'
+$newPwd = 'pwnz0red'
+$oldPwd = 'iForgot'
+
+$secNewPwd = ConvertTo-SecureString –String $newPwd –AsPlainText -Force
+$secOldPwd = ConvertTo-SecureString –String $oldPwd –AsPlainText -Force
+
+Set-ADAccountPassword -Server $Domain -Identity $UserToChange -OldPassword $secOldPwd -NewPassword $secNewPwd 
