3e469dab9d0419fd65492f3846575a817b2567a2
psBrilliance <60415897+psBrilliance@users.noreply.github.com>
2020-12-28 15:47:32 -0600

    Update Get-AdAdmins.ps1

--- Get-AdAdmins.ps1
+++ Get-AdAdmins.ps1
@@ -1,4 +1,4 @@
 Get-ADGroup -Filter * -Server 'knunke.com' | ? { `
     $_.Name -match 'Domain Admins'} | Get-ADGroupMember | % { ` 
-    Get-ADUser -Identity $_.SamAccountNAme -Server 'knunke.com' -Properties * | Select Name, DisplayName, PasswordNeverExpires, CanonicalName, PasswordLastSet, LastLogonDate, Enabled, Created  } | `
-    Export-Csv C:\temp\knunkeAdmins.csv -NoTypeInformation -Force
+    Get-ADUser -Identity $_.SamAccountNAme -Server 'psBrilliance.com' -Properties * | Select Name, DisplayName, PasswordNeverExpires, CanonicalName, PasswordLastSet, LastLogonDate, Enabled, Created  } | `
+    Export-Csv C:\temp\psBrillianceAdmins.csv -NoTypeInformation -Force
